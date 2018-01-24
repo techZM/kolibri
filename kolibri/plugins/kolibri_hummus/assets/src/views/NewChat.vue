087b1bd923b9a97340fd6a3e3212517d0692c123
Christian Memije <christianmemije@gmail.com>
2018-01-24 11:09:38 -0800

    style sucks

--- kolibri/plugins/kolibri_hummus/assets/src/views/NewChat.vue
+++ kolibri/plugins/kolibri_hummus/assets/src/views/NewChat.vue
@@ -72,4 +72,14 @@
 </script>
 
 
-<style lang="stylus"></style>
+<style lang="stylus" scoped>
+
+  tr
+    cursor: pointer
+    &:hover
+      background-color: #eeeeee
+  td
+    padding: 2em
+
+
+</style>
