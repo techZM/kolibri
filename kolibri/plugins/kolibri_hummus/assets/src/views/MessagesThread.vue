5584d2e686f17ad3f6899af9ae2cc8cf2313ee05
Christian Memije <christianmemije@gmail.com>
2018-01-24 01:36:09 -0800

    test

--- kolibri/plugins/kolibri_hummus/assets/src/views/MessagesThread.vue
+++ kolibri/plugins/kolibri_hummus/assets/src/views/MessagesThread.vue
@@ -1,13 +1,41 @@
 <template>
 
   <div class="messages-thread">
-    <div v-if="!currentThread">
+    <div v-if="currentThread">
       No thread selected :(
     </div>
-    <messages-thread-item
-      v-for="thread in currentThread"
-      :key="thread.id"
-    />
+    <template
+      v-else
+    >
+      <div class="messages-thread-header">
+        This is the header and stuff
+      </div>
+
+      <div class="messages-thread-body">
+        bodyy
+        <messages-thread-item
+          v-for="thread in currentThread"
+          :key="thread.id"
+        />
+      </div>
+
+      <div class="messages-thread-input">
+        <k-textbox
+          v-model="message"
+          label="Message"
+          class="message-textbox"
+        />
+        <k-button
+          text="Send"
+          :primary="true"
+          :disabled="message === ''"
+          type="submit"
+        />
+      </div>
+
+
+    </template>
+
   </div>
 
 </template>
@@ -15,13 +43,25 @@
 
 <script>
 
+  import kTextbox from 'kolibri.coreVue.components.kTextbox';
+  import kButton from 'kolibri.coreVue.components.kButton';
+
   export default {
     name: 'MessagesThread',
+    components: {
+      kTextbox,
+      kButton,
+    },
     props: {
       currentThread: {
         type: Array,
         required: false,
       },
+    },
+    data() {
+      return {
+        message: '',
+      };
     },
   };
 
@@ -37,5 +77,28 @@
     height: 100%
     display: inline-block
     vertical-align: top
+    position: relative
+
+  .messages-thread-header
+    height: 48px
+
+  .messages-thread-body
+    position: absolute
+    top: 48px
+    bottom: 72px
+    left: 0
+    right: 0
+    overflow-y: scroll
+
+  .messages-thread-input
+    height: 72px
+    position: absolute
+    bottom: 0
+    left: 0
+    right:0
+
+  .message-textbox
+    display: inline-block
+    vertical-align: middle
 
 </style>
