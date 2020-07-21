0e64b67957a9a96d5a11b30fa4928c0db493dc1e
Kapya Sakala <you@example.com>
2020-07-21 14:55:29 +0200

    duplicated api resources from original learn plugin

--- kolibri/plugins/edulution/assets/src/apiResources.js
+++ kolibri/plugins/edulution/assets/src/apiResources.js
@@ -8,3 +8,24 @@ export const KnowledgeMapResource = new Resource({
     return this.fetchModel({ id, force: true });
   },
 });
+
+/**
+ * Gets all of the Classrooms in which a Learner is enrolled
+ *
+ * @example To get Classrooms without assignments and progress:
+ * LearnerClassroomResource.fetchCollection({
+ *   getParams: { no_assignments: true },
+ * })
+ */
+export const LearnerClassroomResource = new Resource({
+  name: 'learnerclassroom',
+  namespace: 'learnplugin',
+});
+
+/**
+ * Gets Lesson(s) that are assigned to the Learner
+ */
+export const LearnerLessonResource = new Resource({
+  name: 'learnerlesson',
+  namespace: 'learnplugin',
+});
